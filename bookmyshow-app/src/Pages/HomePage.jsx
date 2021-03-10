db334ef1ace17889bd9442ca94a7d35c2821ada5
Naeem <naeemshaik894@gmail.com>
2021-03-10 17:05:53 +0530

    add carousel

---
+++ bookmyshow-app/src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AddCarousel } from "../Components/AddCarousel";
+
+export const HomePage = () => {
+    return (
+        <div>
+            <AddCarousel />
+        </div>
+    )
+}
