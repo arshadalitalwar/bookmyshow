e7e2a695556a3978f58b1d880e8f171d1813b15a
Arshad Ali <arshadalitalwar@gmail.com>
2021-03-11 16:02:17 +0530

    update in movies

--- server/server.js
+++ server/server.js
@@ -139,7 +139,7 @@ const movieSchema = new mongoose.Schema({
   screen_types: { type: Array, required: true },
   languages: { type: Array, required: true },
   movie_duration: { type: String, required: true },
-  rating: { no_of_ratings: { type: Number } },
+  rating: { percentage: { type: Number }, no_of_ratings: { type: Number } },
   about_movie: { type: String, required: true },
   cast: { type: Array, required: true },
   crew: { type: Array, required: true },
