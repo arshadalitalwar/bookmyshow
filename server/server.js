d1bc8ebbbf383db9fceb538d60204a518db13440
Naeem <naeemshaik894@gmail.com>
2021-03-11 16:22:58 +0530

    location

--- server/server.js
+++ server/server.js
@@ -29,17 +29,23 @@ const Booking = mongoose.model("booking", bookingSchema);
 
 app.post("/booking", async (req, res) => {
   const booking = await Booking.create(req.body);
-  res.status(201).json({ data: booking });
+  res.status(201).json({
+    data: booking
+  });
 });
 app.get("/booking", async (req, res) => {
   const booking = await Booking.find({}).lean().exec();
-  res.status(200).json({ data: booking });
+  res.status(200).json({
+    data: booking
+  });
 });
 app.patch("/booking/:id", async (req, res) => {
   const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  res.status(201).json({ data: booking });
+  res.status(201).json({
+    data: booking
+  });
 });
 const locationSchema = new mongoose.Schema({
   cinemas: String,
@@ -53,9 +59,11 @@ const locationSchema = new mongoose.Schema({
 
 const Location = mongoose.model("location", locationSchema);
 
-app.get("/loction", async (req, res) => {
+app.get("/location", async (req, res) => {
   const locations = await Location.find({}).lean().exec();
-  res.status(200).json({ data: locations });
+  res.status(200).json({
+    data: locations
+  });
 });
 const foodSchema = new mongoose.Schema({
   food_name: String,
@@ -70,7 +78,9 @@ const Food = mongoose.model("food", foodSchema);
 
 app.get("/food", async (req, res) => {
   const food = await Food.find({}).lean().exec();
-  res.status(200).json({ data: food });
+  res.status(200).json({
+    data: food
+  });
 });
 const outdoorSchema = new mongoose.Schema({
   image: String,
@@ -82,7 +92,9 @@ const Outdoor = mongoose.model("outdoor", outdoorSchema);
 
 app.get("/outdoor", async (req, res) => {
   const outdoors = await Outdoor.find({}).lean().exec();
-  res.status(200).json({ data: outdoors });
+  res.status(200).json({
+    data: outdoors
+  });
 });
 const laughterSchema = new mongoose.Schema({
   image: String,
@@ -93,7 +105,9 @@ const Laughter = mongoose.model("laughter", laughterSchema);
 
 app.get("/laughter", async (req, res) => {
   const laughters = await Laughter.find({}).lean().exec();
-  res.status(200).json({ data: laughters });
+  res.status(200).json({
+    data: laughters
+  });
 });
 const popularSchema = new mongoose.Schema({
   image: String,
@@ -104,7 +118,9 @@ const Popular = mongoose.model("popular", popularSchema);
 
 app.get("/popular", async (req, res) => {
   const populars = await Popular.find({}).lean().exec();
-  res.status(200).json({ data: populars });
+  res.status(200).json({
+    data: populars
+  });
 });
 const latestSchema = new mongoose.Schema({
   image: String,
@@ -115,7 +131,9 @@ const Latest = mongoose.model("latest", latestSchema);
 
 app.get("/latest", async (req, res) => {
   const latests = await Latest.find({}).lean().exec();
-  res.status(200).json({ data: latests });
+  res.status(200).json({
+    data: latests
+  });
 });
 const cinemaSchema = new mongoose.Schema({
   name: String,
@@ -127,42 +145,94 @@ const Cinema = mongoose.model("cinema", cinemaSchema);
 
 app.get("/cinema", async (req, res) => {
   const cinemas = await Cinema.find({}).lean().exec();
-  res.status(200).json({ data: cinemas });
+  res.status(200).json({
+    data: cinemas
+  });
 });
 
 const movieSchema = new mongoose.Schema({
-  movie_name: { type: String, required: true },
-  cover_url: { type: String, required: true },
-  banner_url: { type: String, required: true },
-  release_date: { type: String, required: true },
-  genre: { type: String, required: true },
-  screen_types: { type: Array, required: true },
-  languages: { type: Array, required: true },
-  movie_duration: { type: String, required: true },
-  rating: { type: String, required: true },
-  about_movie: { type: String, required: true },
-  cast: { type: Array, required: true },
-  crew: { type: Array, required: true },
-  review: { type: Array, required: true },
-  grade: { type: String, required: true },
-  is_primier: { type: Boolean },
+  movie_name: {
+    type: String,
+    required: true
+  },
+  cover_url: {
+    type: String,
+    required: true
+  },
+  banner_url: {
+    type: String,
+    required: true
+  },
+  release_date: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  screen_types: {
+    type: Array,
+    required: true
+  },
+  languages: {
+    type: Array,
+    required: true
+  },
+  movie_duration: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  about_movie: {
+    type: String,
+    required: true
+  },
+  cast: {
+    type: Array,
+    required: true
+  },
+  crew: {
+    type: Array,
+    required: true
+  },
+  review: {
+    type: Array,
+    required: true
+  },
+  grade: {
+    type: String,
+    required: true
+  },
+  is_primier: {
+    type: Boolean
+  },
 });
 
 const Movie = mongoose.model("movie", movieSchema);
 
 app.post("/movies", async (req, res) => {
   const movie = await Movie.create(req.body);
-  res.status(201).json({ data: movie });
+  res.status(201).json({
+    data: movie
+  });
 });
 app.get("/movies", async (req, res) => {
   const movies = await Movie.find({}).lean().exec();
-  res.status(201).json({ data: movies });
+  res.status(201).json({
+    data: movies
+  });
 });
 app.patch("/movies/:id", async (req, res) => {
   const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  res.status(201).json({ data: movie });
+  res.status(201).json({
+    data: movie
+  });
 });
 let port = process.env.PORT || 8000;
 const start = async () => {
